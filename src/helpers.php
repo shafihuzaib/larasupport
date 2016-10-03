62cab8d0ca2ac2413690e425aaf1dabb7ee5766d
shafihuzaib <huzishackey@gmail.com>
2016-10-03 11:46:07 +0530

    Added app_path()

--- src/helpers.php
+++ src/helpers.php
@@ -90,3 +90,16 @@ if (!function_exists('elixir')) {
         throw new InvalidArgumentException("File {$file} not defined in asset manifest.");
     }
 }
+
+if (! function_exists('app_path')) {
+    /**
+     * Get the path to the application folder.
+     *
+     * @param  string  $path
+     * @return string
+     */
+    function app_path($path = '')
+    {
+        return app('path').($path ? DIRECTORY_SEPARATOR.$path : $path);
+    }
+}
